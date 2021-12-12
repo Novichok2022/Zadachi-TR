213b0c82c97a4d2db010045926245aa870bed726
Novichok21 <ivan.ravlinko@outlook.com>
2021-12-13 00:10:53 +0200

    Урок 2

---
+++ Lesson2/Zd3_chast2.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>Задача по регулярках</title>
+    <meta charset="UTF-8">
+    <style>
+   textarea { resize: both; }
+  </style>
+</head>
+<body>
+    
+<form action="" method="POST">
+<textarea name="text" cols="70" rows="15"><?php if (!empty(trim($_POST['text'])))
+    echo trim($_POST['text']);?>
+</textarea>
+	<br><br><input type="submit"><br><br>
+</form>
+
+<?php
+if (!empty(trim($_POST['text']))) 
+
+{
+	$text = trim($_POST['text']);
+        
+        $vstavka_ssilok = preg_replace( "~\b((ftp|https?)://)([a-z0-9-_]+\.)+((([a-z]{2,4})\b/\S*)|([a-z]{2,4})\b(?=\s)?)~i", "<a href = \"$0\">$0</a>", $text);
+        
+	echo "<br> $vstavka_ssilok <br><br>";
+} else {
+    echo 'dВведіть текст з URL для редагування <br>';
+}
+
+?>
+
+
+
+
+
