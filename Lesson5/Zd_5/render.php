7494b76ca3cd6ebfb48892fe47fee469f2ec6394
Ivan R <95438215+Novichok2022@users.noreply.github.com>
2022-01-11 09:24:01 +0200

    --

--- Lesson5/Zd_5/render.php
+++ Lesson5/Zd_5/render.php
@@ -16,22 +16,17 @@ if (isset($_GET['order'])) {
             $order = 0;
         } 
 
+if(!isset($_GET['sort']) && !isset($_GET['order'])) {
+     $products = sortZahluschk($products);
+  }
 
 if($sort == 'price' && $order == 0){
     $products = sortArticleZrostanju($products);
-
 }
 elseif($sort == 'price' && $order == 1){
     $products  = sortArticleSpadanju($products);
-   
-
 }
 
-  if(!isset($_GET['sort']) && !isset($_GET['order'])) {
-      include_once 'render_tovar1.php';
-  } else {
-      include_once 'rennder_tovar2.php';
-} 
-
+  
 
 
